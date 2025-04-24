55594d0e207af890ae18b487a26efac0e6c1ada0
Gustavo <moreiragustavo993@gmail.com>
2025-04-24 19:39:50 -0300

    Probando el LiveServer

--- js/login.js
+++ js/login.js
@@ -1,10 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-    const correo = document.querySelector('input[name="correo"]').value;
-    const password = document.querySelector('input[name="password"]').value;
-  
-    if (!correo || !password) {
-      e.preventDefault();
-      alert("Por favor completa todos los campos.");
-    }
-  });
-  
+  const correo = document.querySelector('input[name="correo"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+
+  if (!correo || !password) {
+    e.preventDefault();
+    alert("Por favor completa todos los campos.");
+  }
+});
